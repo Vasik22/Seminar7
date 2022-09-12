b307323745d545f9bf8f591210223a4bdfef48d2
Your Name <you@example.com>
2022-09-12 22:12:32 +0300

    needcommit

---
+++ Program.cs
@@ -0,0 +1,153 @@
+﻿
+// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+
+// Console.Write("Введите количество строк массива: ");
+// int m = int.Parse(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+// int n = int.Parse(Console.ReadLine());
+// double[,] array = new double[m, n];
+// FillArrayRandomarray(array);
+// PrintArray(array);
+
+// void FillArrayRandomarray(double[,] array)
+// {
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+// for (int j = 0; j < array.GetLength(1); j++)
+// {
+// array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+// }
+// }
+// }
+// void PrintArray(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.Write("[ ");
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.Write("]");
+//         Console.WriteLine("");
+//     }
+// }
+
+//конец
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+// Console.Write("Введите строку:");
+// int m = int.Parse(Console.ReadLine());
+// Console.Write("Введите столбец:");
+// int n = int.Parse(Console.ReadLine());
+// int[,] array = new int[10, 10];
+// FillArrayRandomarray(array);
+// PrintArray(array);
+
+
+// if (n > array.GetLength(0) || m > array.GetLength(1))
+// {
+//     Console.WriteLine("такого элемента нет");
+// }
+// else
+// {
+//     Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {array[n - 1, m - 1]}");
+// }
+
+
+// void FillArrayRandomarray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(-100, 100) / 10;
+//         }
+//     }
+// }
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.Write("[ ");
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.Write("]");
+//         Console.WriteLine("");
+//     }
+// }
+// //конец
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+// Console.Write("Введите количество строк массива: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[,] array = new int[m, n];
+// FillArrayRandomarray(array);
+// PrintArray(array);
+
+// void FillArrayRandomarray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(-100, 100) / 10;
+//         }
+//     }
+// }
+// for (int i=0;i<n;i++)
+//             {
+//                 int summ = 0;
+//                 for (int j=0;j<m;j++)
+//                 {
+//                     summ = summ + array[j,i];
+//                 }
+//                 int average = summ / 3;
+//                 int a = i + 1;
+//                 Console.WriteLine("Avarage for " +a+" column is " + average);
+//             }
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.Write("[ ");
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i,j] + " ");
+//         }   
+//         Console.Write("]");
+//         Console.WriteLine(""); 
+//     }
+// }
+// конец
